ee4301bb0cfd004a149f2ae0f4be940a69d3f57e
william <willfmaga@gmail.com>
2023-02-08 16:51:37 -0300

    com botao e capturando a action

--- organo/src/componentes/Formulario/index.js
+++ organo/src/componentes/Formulario/index.js
@@ -1,14 +1,35 @@
 import "./Formulario.css"
 import CampoTexto from "../CampoTexto"
+import ListaSuspensa from "../ListaSuspensa"
+import Botao from "../Botao"
 
 const Formulario = () => {
+
+    const times = [
+        'Programação',
+        'Front-End',
+        'Data Sciente',
+        'Devops',
+        'UX e Design',
+        'Mobile',
+        'Inovação e Gestão'
+    ]    
+    
+    const aoSalvar = (evento) =>{
+        evento.preventDefault()
+        console.log("Form foi submetido")
+    }
     return (
         <section className="formulario">
-            <form>
+            <form onSubmit={aoSalvar}>
                 <h2>Preencha os dados para criar o card do colaborador</h2>
-                <CampoTexto label="Nome" placeholder="Digite o seu nome" />
-                <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
+                <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite o seu nome" />
+                <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite seu cargo" />
                 <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
+                <ListaSuspensa obrigatorio={true} label="Time" itens={times}></ListaSuspensa>
+                <Botao >
+                    Criar Card
+                </Botao>
             </form>
         </section>
     )
